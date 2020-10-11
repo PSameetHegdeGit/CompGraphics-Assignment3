6946c53d00de775c67abb40d9f8d77a6e33adb19
PSameetHegdeGit <sameethegde123@gmail.com>
2020-10-10 23:20:38 -0400

    commiting some stuff

---
+++ Assets/Scripts/PersonLogic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonLogic : MonoBehaviour
+{
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        control();
+
+    }
+
+    public void control()
+    {
+
+
+        var direction = transform.forward;
+        var x_vel = Input.GetAxis("Horizontal");
+        var y_vel = Input.GetAxis("Vertical");
+
+        print(x_vel);
+        print(y_vel);
+
+        anim.SetFloat("XVelocity", x_vel);
+        anim.SetFloat("YVelocity", y_vel);
+    }
+
+
+}
