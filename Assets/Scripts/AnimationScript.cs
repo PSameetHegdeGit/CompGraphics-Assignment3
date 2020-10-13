f29fa9c757e45852501f11889ea387ca11584665
PSameetHegdeGit <sameethegde123@gmail.com>
2020-10-13 12:19:06 -0400

    added jump

--- Assets/Scripts/AnimationScript.cs
+++ Assets/Scripts/AnimationScript.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class AnimationScript : MonoBehaviour
 {
     private Animator anim;
+    private float run = 0;
+
+    public Transform target;
+
+    public float timescale;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +21,34 @@ public class AnimationScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
         float x_vel = Input.GetAxis("Horizontal");
         float y_vel = Input.GetAxis("Vertical");
 
+  
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            run = Mathf.Clamp01(run + 1 * Time.deltaTime);
+        }
+        else
+        {
+            run = Mathf.Clamp01(run - 1 * Time.deltaTime);
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Jump");
+        }
+    
+
+
         anim.SetFloat("x_vel", x_vel);
         anim.SetFloat("y_vel", y_vel);
+        anim.SetFloat("Run", run);
+
     }
 }
