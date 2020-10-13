a586dfb134ca35d023343456131c8186b796e589
Abhijit Bhatt <Abhijit.Bhatt@gmail.com>
2020-10-12 21:20:38 -0400

    WASD controllable animation

---
+++ Assets/Scripts/AnimationScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x_vel = Input.GetAxis("Horizontal");
+        float y_vel = Input.GetAxis("Vertical");
+
+        anim.SetFloat("x_vel", x_vel);
+        anim.SetFloat("y_vel", y_vel);
+    }
+}
