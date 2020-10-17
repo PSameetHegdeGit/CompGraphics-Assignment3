b02609393a6b94225d210d4fe723a39ecdf707a5
PSameetHegdeGit <sameethegde123@gmail.com>
2020-10-17 12:51:15 -0400

    moving character

--- Assets/Scripts/AnimationScript.cs
+++ Assets/Scripts/AnimationScript.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 public class AnimationScript : MonoBehaviour
 {
     private NavMeshAgent agent;
-    private string groundTag = "Ground";
+    private string groundTag = "ground";
 
     // Start is called before the first frame update
     void Start()
