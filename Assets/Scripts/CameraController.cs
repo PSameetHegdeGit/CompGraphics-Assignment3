cdecb5be0930c4bf969fbb44bad3e1f48c4df2a3
PSameetHegdeGit <sameethegde123@gmail.com>
2020-10-19 13:30:06 -0400

    part1 ready

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = (player.transform.position - transform.position) /2;
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float y = Input.GetAxis("Horizontal");
+       float displacement = player.transform.eulerAngles.y;
+       Quaternion rotation = Quaternion.Euler(0, displacement, 0);
+       transform.position = player.transform.position - (rotation * offset);
+       transform.LookAt(player.transform);
+    }
+}
